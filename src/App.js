9bf386debcf93d40e8175f0ab4ddf108863c1c53
Jack Burgess <198895932+jackth@users.noreply.github.com>
2020-05-09 16:27:55 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -6,65 +6,105 @@ import Chart from 'chart.js';
 
 import moment from 'moment';
 
-async function fetchAsync (url) {
-    const response = await fetch(url);
-    return await response.json();
-  }
 export default class App extends React.Component {
     constructor(){
         super();
         this.state = {
-            labels: [],
-            data: [0, 1, 2],
+          labels: [],
+          cases: [],
+          daily: []
         };
+        this.daily()
     }
-  componentDidMount() {
-    fetchAsync('https://api.covid19api.com/dayone/country/united-kingdom/status/confirmed').then(result => {
+  async componentDidMount() {
+    (await fetch('https://api.covid19api.com/dayone/country/united-kingdom')).json().then(result => {
         this.setState({
             labels : result.filter(item => item.Province === '').map(element => element.Date), 
-            data: result.filter(item => item.Province === '').map(element => element.Cases),
-        })
+            cases: result.filter(item => item.Province === '').map(element => element.Confirmed),
+            deaths: result.filter(item => item.Province === '').map(element => element.Deaths),
+          })
     })
   }
   componentDidUpdate(prevProps, prevState){
-      if (prevState.data !== this.state.data) {
+      if (prevState.cases !== this.state.cases) {
+        this.daily()
+        this.drawGraph2()
+      } else if (prevState.daily !== this.state.daily) {
         this.drawGraph2()
       }
   }
+  increase() {
+    const data = this.state.cases
+    const length = data.length
+    return data[length-1] - data[length-2]
+  }
+  daily() {
+    const data = this.state.cases
+    let arr = [];
+    for (let i = 0; i < data.length-1; i++) {
+        arr.push(data[i]-data[i-1])
+    }
+    console.log(arr)
+    this.setState({daily: arr})
+    console.log(this.state.daily)
+    
+  }
   drawGraph2(){
-    var ctx = document.getElementById('myChart2').getContext('2d');
+    let ctx = document.getElementById('myChart2').getContext('2d');
     new Chart(ctx, {
         type: 'line',
         data: {
-            // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             labels: this.state.labels.map(date => moment(date).format("MMMM Do")),
-            datasets: [{
+            datasets: [
+              {
+                label: 'Deaths',
+                data: this.state.deaths,
+                backgroundColor: "rgba(141, 12, 12, 0.5)",
+                fill: true
+              },
+              {
                 label: 'Cases',
-                data: this.state.data,
+                data: this.state.cases,
                 backgroundColor: "rgba(230, 211, 255, 0.7)",
                 fill: true
-            }]
+              },
+              {
+                label: 'Daily Cases',
+                data: this.state.daily,
+                backgroundColor: "rgba(230, 211, 255, 0.7)",
+                type: 'bar',
+                fill: true
+              }
+          ]
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
     });
   }
+
   render() {
     return (
       <div className="App">
-          {console.log(this.state.labels[this.state.labels.length-1])}
           <h1 className="title">Current total cases in the United Kingdom as of: {moment(this.state.labels[this.state.labels.length-1]).format('h:mm a, Do MMMM')}</h1>
           <div className="chart">
             <canvas id="myChart2" width="400" height="400"></canvas>
           </div>
+        <p>The total increase from yesterday was + {this.increase()}.</p>
+        <div>
+          <h1>Cases: {this.state.cases[this.state.labels.length-1]}</h1>
+        </div>
       </div>
     );
   }
 }
+
+
+// { DATASETS
+//   label: 'Cases',
+//   data: this.state.data,
+//   backgroundColor: "rgba(230, 211, 255, 0.7)",
+//   fill: true
+// }, {
+//   label: 'Deaths',
+//   data: this.state.deaths,
+//   backgroundColor: "rgba(141, 12, 12, 0.5)",
+//   fill: true
+// },
